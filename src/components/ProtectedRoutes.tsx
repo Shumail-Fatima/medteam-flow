a46ebdc08984d50c15afd53739a34f4a8687abbe
Shumail-Fatima <shumialarshabubit@gmail.com>
2025-06-19 01:30:14 -0700

    update ProtectedRoute to redirect unauthenticated users to the home page

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -14,7 +14,7 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
   const { isAuthenticated, isAdmin } = useAuth();
     // Check if the user is authenticated
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" replace />;
   }
 
   if (requireAdmin && !isAdmin) {
